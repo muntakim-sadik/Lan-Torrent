021ac991c929b96620fe972c09f9f344f88e4ba2
Muntakim <muntakim.sadik@yahoo.com>
2011-12-13 00:12:12 +0600

    client update

---
+++ src/lantorrent/TorrentFileProcessor.java
@@ -0,0 +1,76 @@
+/**********************************************************************************
+ * This class makes a torrent file for a given file class
+ * or extracts data form a torrent file and returns a
+ * FileStructure class which defines the fileName,
+ * size, segment details
+ * @completed
+ */
+
+package lantorrent;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class TorrentFileProcessor {
+    FileStructure struct;
+    File f;
+    String defaultTrackerIP="localhost";
+    int defaultTrackerPort = 10000;
+
+    TorrentFileProcessor(String s){
+        f = new File(s);
+    }
+
+    /// here argument f is the File instance to save the torrent file
+    void makeTorrentFile(File f) throws IOException{
+        ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream(f));
+        ArrayList<Range> x = new ArrayList();
+        int segSize;
+        if(f.length()<=5*1024*1024) //if less than 5MB transfer as whole
+            segSize =5*1024*1024;
+        else if(f.length()<=1024*1024*1024) //if less than 1GB trasfer as segments of 10mb each
+                segSize =10*1024*1024;
+        else segSize = 20*1024*1024; // else transfer as segments of 20Mb each;
+
+        int tot = (int)Math.ceil((double)f.length()/segSize);
+        for(long i=0;i<tot-1;i++)
+            x.add(new Range(i,(i+1)*segSize-1));
+        if(!(f.length()%tot==0))
+            x.add(new Range(x.get(x.size()).to+1,f.length()));
+
+        struct = new FileStructure(f.getName(),f.length(),tot,segSize,x,defaultTrackerIP,defaultTrackerPort);
+
+        oout.writeObject(struct);
+        oout.close();
+    }
+
+    /* return the list of segments of the file initialized with "has" signal marked as false*/
+    FileStructure process() throws IOException,ClassNotFoundException {
+        ObjectInputStream oin = new ObjectInputStream(new FileInputStream(f));
+        struct = (FileStructure)oin.readObject();
+
+        return struct;
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class FileStructure implements Serializable{
+    String fileName;
+    long fileSize; ///in bytes
+    int totalSegments;
+    int segmentSize;
+    String trackerIP;
+    int trackerPort;
+    ArrayList<Range> segmentList;
+    FileStructure(String s, long l, int i1, int i2,ArrayList<Range> r,String s2, int p){
+        fileName = s;
+        fileSize = l;
+        totalSegments = i1;
+        segmentSize = i2;
+        segmentList = r;
+        trackerIP = s2;
+        trackerPort = p;
+    }
+}
---
+++ src/lantorrent/Update.java
@@ -0,0 +1,93 @@
+package lantorrent;
+
+import java.util.ArrayList;
+import java.util.Properties;
+import java.sql.*;
+
+public class Update {
+	String preprocess(ArrayList<Range> l) {
+		String ret = "";
+		for (Range r : l) {
+			ret += r.from + "-" + r.to + ", ";
+		}
+		return ret;
+	}
+
+	private static final String dbClassName = "com.mysql.jdbc.Driver";
+	private static final String CONNECTION = "jdbc:mysql://127.0.0.1/LanTorrent";
+
+	Update(String ip, int port, String fileName, ArrayList<Range> r)
+			throws ClassNotFoundException, SQLException {
+		try {
+			Class.forName(dbClassName);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Properties properties = new Properties();
+		properties.put("user", "root");
+		properties.put("password", "s");
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(CONNECTION, properties);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Statement st = connection.createStatement();
+		st = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+		ResultSet uprs = st
+				.executeQuery("select * from LanTorrent.data where name='"
+						+ fileName + "' and ip='" + ip + "' and port='" + port
+						+ "'");
+
+		while (uprs.next()) {
+			
+			String up = preprocess(r);
+			//System.out.println(up);
+			uprs.updateString("DOWNLOADED", up);
+			uprs.updateRow();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Query q = null;
+		try {
+			q = new Query("file.pdf");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int ii=0;
+		ArrayList<QueryData> data = q.get();
+		for (QueryData qd : data) {
+			//System.out.println(ii++);
+			//System.out.print(qd.ip + " " + qd.port + " ");
+			for (int i = 0; i < qd.segments.size(); i++) {
+				qd.segments.get(i).from +=12;
+				qd.segments.get(i).to +=12;
+				//System.out.println(qd.segments.get(i).from);
+				//System.out.println(qd.segments.get(i).to);
+			}
+			try {
+				Update up=new Update(qd.ip,qd.port,"file.pdf",qd.segments);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// System.out.println(qd.segments.size());
+			//System.out.println();
+		}
+		
+
+	}
+}
